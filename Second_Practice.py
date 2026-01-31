d06d375abf0d2ec899eaf938be998097f1eb8f6a
BrijeshPatil <113664505+16Br04@users.noreply.github.com>
2026-01-31 19:35:03 +0530

    Add files via upload

---
+++ Second_Practice.py
@@ -0,0 +1,7 @@
+A = int(input("Enter the value for A: "))
+B = float(input("Enter the value for B: "))
+C = int(input("Enter the value for C: "))
+D = float(input("Enter the value for D: "))
+Ans = (A + B) * C / D
+print("The result of the expression (A + B) * C / D is:", Ans)
+
